396f230ec1753530926989b79ab939ab7a41f57a
Olavo-marques-do-nascimento <omarquesdonascimento@gmail.com>
2022-05-22 19:15:44 -0300

    Atualização-projeto-Blackjack

--- modulo1/projeto-blackjack/nossoJogo.js
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -11,40 +11,81 @@
  * 
  */
 
-console.log("Boas vindas a o jogo de Blackjack, você está pronto?")
 
 
-if(confirm("Quer iniciar uma nova rodada?")) {
+if(confirm("BOAS VINDAS AO JOGO BLACKJACK, VOCÊ ESTÁ PRONTO?")) {
+   
+   console.log(``)
+   console.log("PRIMEIRA RODADA!")
+   console.log(``)
+
+console.log(`♦️ ♥️ ♣️ ♠️ ♦️ ♥️ ♣️ ♠️ ♦️ ♥️ ♣️ ♠️`)
 
 const usuario = comprarCarta()
-console.log(usuario.texto),
-console.log(usuario.valor)
 const usuario2 = comprarCarta()
-console.log(usuario2.texto)
-console.log(usuario2.valor)
+const somaUsuario = usuario.valor + usuario2.valor
+console.log(`Usuario\nSuas cartas: ${usuario.texto}, ${usuario.valor};  ${usuario2.texto}, ${usuario2.valor}.\nPontuação: ${somaUsuario}. `)
 
-
-
+console.log(`♦️ ♥️ ♣️ ♠️ ♦️ ♥️ ♣️ ♠️ ♦️ ♥️ ♣️ ♠️`)
 
 const pc = comprarCarta()
-console.log(pc.texto)
-console.log(pc.valor)
 const pc2 = comprarCarta()
-console.log(pc.texto)
-console.log(pc.valor)
+const somaPc = pc.valor + pc2.valor
+console.log(`Máquina\nMinhas cartas: ${pc.texto}, ${pc.valor};  ${pc2.texto}, ${pc2.valor}.\nPontuação: ${somaPc}.`)
+
+console.log(`♦️ ♥️ ♣️ ♠️ ♦️ ♥️ ♣️ ♠️ ♦️ ♥️ ♣️ ♠️`)
 
 
+console.log(` `)
+
+if (somaUsuario > somaPc) {
+   console.log(`VOCÊ VENCEU!`)
+
+} else if (somaUsuario === somaPc) {
+   console.log(`EMPATE.`) 
 
 } else {
-	console.log("O jogo acabou. Mas volte quando quiser um desafio.")
+   console.log(`VOCÊ PERDEU.`)   
+
+}
+} else {
+	console.log("O JOGO ACABOU. MAS VOLTE QUANDO QUISER UM NOVO DESAFIO.")
+
 }
 
+//=====================================================================================
+//=====================================================================================
 
-// const carta = comprarCarta(); 
-// // Sorteia uma carta. Por exemplo, o rei de ouros
+// //DESAFIO
 
-// console.log(carta.texto) 
-// // imprime o texto da carta. Nesse caso: "K♦️"
+// if(confirm("BOAS VINDAS AO JOGO BLACKJACK, VOCÊ ESTÁ PRONTO?")) {
 
-// console.log(carta.valor) 
-// // imprime o valor da carta (um número). Nesse caso: 10)
+
+// const usuario = comprarCarta()
+// const usuario2 = comprarCarta()
+// const somaUsuario = usuario.valor + usuario2.valor
+// alert(`♦️ ♥️ ♣️ ♠️ ♦️ ♥️ ♣️ ♠️ ♦️ ♠️\nUsuario\nSuas cartas são: ${usuario.texto},  ${usuario2.texto}. \nPontuação: ${somaUsuario}\n♦️ ♥️ ♣️ ♠️ ♦️ ♥️ ♣️ ♠️ ♦️ ♠️ `)
+
+// const pc = comprarCarta()
+// const pc2 = comprarCarta()
+// const somaPc = pc.valor + pc2.valor
+// alert(`♦️ ♥️ ♣️ ♠️ ♦️ ♥️ ♣️ ♠️ ♦️ ♠️\nMáquina\nMinhas cartas são:  ${pc.texto},  ${pc2.texto}. \nPontuação: ${somaPc}\n♦️ ♥️ ♣️ ♠️ ♦️ ♥️ ♣️ ♠️ ♦️ ♠️`)
+
+// //========================================================================
+// console.log(` `)
+
+// if (somaUsuario > somaPc) {
+//    alert(`VOCÊ VENCEU!`)
+
+// } else if (somaUsuario === somaPc) {
+//    alert(`EMPATE.`) 
+   
+// } else {
+//    alert(`VOCÊ PERDEU.`)   
+
+// }
+// } else {
+// 	alert("O JOGO ACABOU. MAS VOLTE QUANDO QUISER UM NOVO DESAFIO.")
+
+// }
+
