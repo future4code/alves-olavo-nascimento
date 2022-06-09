c6da96c0a0364ef3a5bb86e316ae5af928a2104f
Olavo-marques-do-nascimento <omarquesdonascimento@gmail.com>
2022-06-08 22:52:00 -0300

    Exercício de estado

---
+++ modulo2/estados-insta4/insta4/src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from 'styled-components'
+import Post from './components/Post/Post';
+
+const MainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`
+
+class App extends React.Component {
+  render() {
+    return (
+      <MainContainer>
+        <Post
+          nomeUsuario={'paulinha'}
+          fotoUsuario={'https://picsum.photos/50/50'}
+          fotoPost={'https://picsum.photos/200/150'}
+        />
+        <Post
+          nomeUsuario={'pedro'}
+          fotoUsuario={'https://picsum.photos/60/60'}
+          fotoPost={'https://picsum.photos/200/160'}
+        />
+        <Post
+          nomeUsuario={'patrick'}
+          fotoUsuario={'https://picsum.photos/70/70'}
+          fotoPost={'https://picsum.photos/200/170'}
+        />
+      </MainContainer>
+    );
+  }
+}
+
+export default App;
